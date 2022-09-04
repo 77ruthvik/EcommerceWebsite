dee50e8d15bfe0c9deefee23b3294554a167088a
77ruthvik <74626667+77ruthvik@users.noreply.github.com>
2022-09-04 17:32:50 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const customerRoutes = require('./routes/customer');
 const { urlencoded } = require('express');
 
 //settings
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001); //3000
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
